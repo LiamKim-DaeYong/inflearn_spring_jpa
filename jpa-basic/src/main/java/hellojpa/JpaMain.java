72722e48dc12f4fc139eb7487281e727cae8b3df
liamkim <liamkim1018@gmail.com>
2022-04-06 01:00:50 +0900

    영속성 컨텍스트

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -15,13 +16,21 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //비영속
             Member member = new Member();
-            member.setId(1L);
+            member.setId(101L);
             member.setName("HelloA");
-            em.persist(member);
 
-            Member findMember = em.find(Member.class, 2L);
-            findMember.setName("HelloJPA");
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
